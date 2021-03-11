67f9abaa0739dde18d0f955bb303741110d17a1c
Alex Willisson <alex.willisson@gmail.com>
2021-03-11 16:49:57 -0500

    creates simple article list from local file

--- src/app/articles-list/articles-list.component.ts
+++ src/app/articles-list/articles-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { articles } from "../articles";
+
 @Component({
   selector: 'app-articles-list',
   templateUrl: './articles-list.component.html',
   styleUrls: ['./articles-list.component.css']
 })
 export class ArticlesListComponent implements OnInit {
+  articles = articles;
 
   constructor() { }
 
---
+++ src/app/articles.ts
@@ -0,0 +1,12 @@
+export const articles = [
+  {
+    id: 42,
+    title: "The answer to life, the universe, and everything",
+    abstract: "Maybe it's 6 * 7?"
+  },
+  {
+    id: 1234,
+    title: "The question to life, the universe, and everything",
+    abstract: "We're excited to present to you a bunch of gibberish that looks like we're using our current grants well and can be leveraged to ask for more grant money while we try to figure this thing out"
+  }
+]
