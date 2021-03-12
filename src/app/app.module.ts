b271af6fdb842184ed470ab650f9cf1cb08fb3c6
Alex Willisson <alex.willisson@gmail.com>
2021-03-12 02:14:30 -0500

    adds material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { AbstractComponent } from './articles-list/abstract/abstract.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AbstractComponent } from './articles-list/abstract/abstract.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
